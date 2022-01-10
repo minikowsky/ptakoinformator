756d59c53fcc1b5ebb099b556490a30532740b93
BartoszPolekPolSl <bartek.polek@wp.pl>
2022-01-10 20:02:09 +0100

    Add selecting photo from gallery

--- app/src/main/java/com/example/ptakoinformator/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/ptakoinformator/fragments/HomeFragment.kt
@@ -1,18 +1,20 @@
 package com.example.ptakoinformator.fragments
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.ptakoinformator.R
 import com.example.ptakoinformator.databinding.HomeFragmentBinding
 
 
 class HomeFragment : Fragment() {
+
 
     private var _binding: HomeFragmentBinding? = null
     private val binding get() = _binding!!
@@ -31,7 +33,21 @@ class HomeFragment : Fragment() {
         binding.buttonShowReports.setOnClickListener{
             it.findNavController().navigate(HomeFragmentDirections.actionFragmentHomeToFragmentReports())
         }
+        binding.buttonUploadPhoto.setOnClickListener { pickImageGallery() }
     }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getImage.launch(intent)
+    }
+    private val getImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == Activity.RESULT_OK) {
+            val selectedImage = it.data?.data
+            binding.imageViewSelectedImage.setImageURI(selectedImage)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
