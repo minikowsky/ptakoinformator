258a351c307fe03b07c0be91979bcfcc3d5107a6
Dominik Marszalek <marszalek.dominik@gmail.com>
2022-01-17 21:40:17 +0100

    hehe

--- app/src/main/java/com/example/ptakoinformator/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/ptakoinformator/fragments/HistoryFragment.kt
@@ -1,23 +1,31 @@
 package com.example.ptakoinformator.fragments
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
+import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ptakoinformator.R
+import com.example.ptakoinformator.data.Bird
 import com.example.ptakoinformator.viewmodels.HistoryViewModel
 import com.example.ptakoinformator.viewmodels.HistoryViewModelFactory
+import java.io.File
+import java.util.*
 
 class HistoryFragment: Fragment() {
 
@@ -53,8 +61,47 @@ class HistoryFragment: Fragment() {
 
         view.findViewById<Button>(R.id.button_export_html).setOnClickListener {
             //TODO: Generate html file
+
+            val htmlText: String = getHtmlText(viewModel.birds)
+
+
+            Log.println(Log.INFO,"html save","beginning of creating")
+            File.createTempFile(
+                "raport_${Date()}",
+                ".html",requireContext()
+                    .getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)).apply {
+                        this.appendText(htmlText)
+                Log.println(Log.INFO,"html save","creating")
+            }
+            Log.println(Log.INFO,"html save","end of creating")
         }
     }
+
+    private fun getHtmlText(birds: LiveData<List<Bird>>): String {
+        var s: String = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><html>\n" +
+                "<head>\n" +
+                "<title>Hello world</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<header>Raport z dnia ${Date()}</header>" +
+                "<table>\n" +
+                "<td>"
+        birds.value?.forEach {
+            s+="<tr><p>${it.date}<img src=\"${it.photoUri}\" alt=\"${it.photoUri}\" width=\"500\" height=\"500\">"
+            s+="<p>Klasyfikacja:</p>"
+            s+="<p>Największe prawdopodobieństwo: ${it.classification.mainClassification}-${it.classification.mainProbability}</p>"
+            s+="<p>Drugie najwyższe: ${it.classification.secondClassification}-${it.classification.secondProbability}</p>"
+            s+="<p>Trzecie najwyższe: ${it.classification.thirdClassification}-${it.classification.thirdProbability}</p></p></tr>"
+        }
+
+        s += "</td>\n</table>\n</body>\n</html>"
+        return s
+    }
+
+    private fun saveHtmlFileToStorage(htmlText: String){
+        val fileCollection = MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    }
+
 
     /*fun createFile(pickerInitialUri: Uri){
         val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
